2fe81c3231dd391874af712fa31c86586256c88d
elliottdobbs <elliottdobbs95@gmail.com>
2017-09-30 13:19:16 -0500

    roblem 1 complete, working on 2

--- HW1/hw1pr1.cpp
+++ HW1/hw1pr1.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <math.h>
 #include <float.h>
+#include <stdlib.h>
 using namespace std;
 
 struct Point{
@@ -16,6 +17,11 @@ struct Point{
     int y;
     bool checked;
 };
+
+double pointDist(Point p1, Point p2){
+    return sqrt((p1.x - p2.x)*(p1.x - p2.x) +
+                (p1.y - p2.y)*(p1.y - p2.y));
+}
 
 int closestPoint(Point p[], int iter){
     
@@ -26,14 +32,13 @@ int closestPoint(Point p[], int iter){
     //Checks the distance between the given point (via iter)
     // and all other non visited points to return the iteration
     // location of the closest point
-    while(p[i].x != -1){
+    while(p[i].x != -1 && i < 10000){
         if (p[i].checked){
             ++i;
             continue;
         }
         
-        tempDis = sqrt((p[i].x - p[iter].x)*(p[i].x - p[iter].x) +
-                       (p[i].y - p[iter].y)*(p[i].y - p[iter].y));
+        tempDis = pointDist(p[i], p[iter]);
         
         if (distance > tempDis){
             distance = tempDis;
@@ -46,7 +51,7 @@ int closestPoint(Point p[], int iter){
 }
 
 int main(){
-    
+
     //setting up point array
     Point p;
     p.x = -1;
@@ -55,9 +60,17 @@ int main(){
     Point points[10000];
     fill_n(points, 10000, p);
     
+    //FOR GENERATIUNG RANDOM POINTS
+//    ofstream myfile1;
+//    myfile1.open("hw1pr1_data.txt");
+//    for (int r = 0; r < 10000; r++){
+//        myfile1 << rand() % 1000 << " " << rand() % 1000 << '\n';
+//    }
+//    myfile1.close();
+    
     //getting input file into Points array structure
     ifstream myfile;
-    myfile.open ("hw1pr1_data.txt");
+    myfile.open("hw1pr1_data.txt");
     int i = 0;
     int num;
     while(myfile >> num){
@@ -81,19 +94,13 @@ int main(){
             break;
         
         points[closestP].checked = true;
-        totalDist += sqrt((points[closestP].x - points[i].x)*
-                          (points[closestP].x - points[i].x) +
-                          (points[closestP].y - points[i].y)*
-                          (points[closestP].y - points[i].y));
+        totalDist += pointDist(points[closestP], points[i]);
         i = closestP;
     }
     
     //Once all points are checked, add on the last distance of the last
     //  visited point to the starting points
-    totalDist += sqrt((points[0].x - points[i].x)*
-                      (points[0].x - points[i].x) +
-                      (points[0].y - points[i].y)*
-                      (points[0].y - points[i].y));
+    totalDist += pointDist(points[0], points[i]);
     
     //Display the result
     cout << "\nThe distance of the greedy best-first search algorithm solution is: " << totalDist << endl << endl;
--- HW1/hw1pr2.cpp
+++ HW1/hw1pr2.cpp
@@ -17,11 +17,17 @@ struct Point{
     bool checked;
 };
 
-int closestPoint(Point p[], int iter){
+double pointDist(Point p1, Point p2){
+    return sqrt((p1.x - p2.x)*(p1.x - p2.x) +
+                (p1.y - p2.y)*(p1.y - p2.y));
+}
+
+int* fourClosestPoints(Point p[], int iter){
     
     int i = 0;
-    int result = -1;
-    double distance = DBL_MAX, tempDis;
+    int *result = new int[4];
+    result[0] = -1; result[1] = -1; result[2] = -1; result[3] = -1;
+    double tempDis;
     
     //Checks the distance between the given point (via iter)
     // and all other non visited points to return the iteration
@@ -32,13 +38,59 @@ int closestPoint(Point p[], int iter){
             continue;
         }
         
-        tempDis = sqrt((p[i].x - p[iter].x)*(p[i].x - p[iter].x) +
-                       (p[i].y - p[iter].y)*(p[i].y - p[iter].y));
+        tempDis = pointDist(p[i], p[iter]);
         
-        if (distance > tempDis){
-            distance = tempDis;
-            result = i;
+        //Check if there is nothing in the spot
+        if (result[0] == -1){
+            result[0] = i;
+            p[i].checked = true;
         }
+        //if there is something, check if its shorter than the found one
+        else if (tempDis < pointDist(p[result[0]], p[iter]){
+            
+            //if we have a new shortest distance, shift the
+            // rest of the shortest points to make room
+            result[3] = result[2];
+            result[2] = result[1];
+            result[1] = result[0];
+            result[0] = i;
+        }
+        //repeat with the other locations in result
+        else if (result[1] == -1){
+            result[1] = i;
+            p[i].checked = true;
+        }
+                 //keep replacing the sqrt with enw function
+        else if (tempDis < sqrt((p[result[1]].x - p[iter].x)*
+                                (p[result[1]].x - p[iter].x) +
+                                (p[result[1]].y - p[iter].y)*
+                                (p[result[1]].y - p[iter].y))){
+            result[3] = result[2];
+            result[2] = result[1];
+            result[1] = i;
+        }
+        else if (result[2] == -1){
+            result[2] = i;
+            p[i].checked = true;
+        }
+        else if (tempDis < sqrt((p[result[2]].x - p[iter].x)*
+                                (p[result[2]].x - p[iter].x) +
+                                (p[result[2]].y - p[iter].y)*
+                                (p[result[2]].y - p[iter].y))){
+            result[3] = result[2];
+            result[2] = i;
+        }
+        else if (result[3] == -1){
+            result[3] = i;
+            p[i].checked = true;
+        }
+        else if (tempDis < sqrt((p[result[3]].x - p[iter].x)*
+                                (p[result[3]].x - p[iter].x) +
+                                (p[result[3]].y - p[iter].y)*
+                                (p[result[3]].y - p[iter].y))){
+            result[3] = i;
+        }
+        
         ++i;
     }
     
@@ -75,11 +127,25 @@ int main(){
     i = 0;
     double totalDist = 0.0;
     
+    //maybe recursive function here instead of loop
+    /*
+     so here we have
+         closestPoints : the 4 closest points of the start point
+         points[] : with all the points and only first point marked as checked
+     
+     
+     -a recursion will have base cases, closestPoints size 1
+         -this means we are at a last point
+         -so we need send the distance from this node to the start node,
+         -(and the dist from the prev to this node?)
+     -we return the distance of
+     */
     while (true){
-        int closestP = closestPoint(points, i);
-        if (closestP == -1)
-            break;
+        int *closestPoints = fourClosestPoints(points, i);
         
+        if (closestPoints[0] == -1)
+            break;
+
         points[closestP].checked = true;
         totalDist += sqrt((points[closestP].x - points[i].x)*
                           (points[closestP].x - points[i].x) +
@@ -87,14 +153,14 @@ int main(){
                           (points[closestP].y - points[i].y));
         i = closestP;
     }
-    
+
     //Once all points are checked, add on the last distance of the last
     //  visited point to the starting points
     totalDist += sqrt((points[0].x - points[i].x)*
                       (points[0].x - points[i].x) +
                       (points[0].y - points[i].y)*
                       (points[0].y - points[i].y));
-    
+
     //Display the result
     cout << "\nThe distance of the greedy best-first search algorithm solution is: " << totalDist << endl << endl;
     
