2847f9f739f53fa1df4df5c6d00efbb62da62a1b
elliottdobbs <elliottdobbs95@gmail.com>
2017-10-04 10:53:44 -0500

    hw1pr3.cpp working. Test on build.tamu.edu

--- HW1/hw1pr3.cpp
+++ HW1/hw1pr3.cpp
@@ -1,6 +1,6 @@
 //Elliott Dobbs 823004322
 //CSCE 420
-//Due: October 2, 2017
+//Due: October 4, 2017
 //hw1pr3.cpp
 
 #include <stdio.h>
@@ -10,9 +10,9 @@
 #include <vector>
 #include <math.h>
 #include <iomanip>
-#include <unistd.h>
 using namespace std;
 
+//Move functions for a given puzzle state, and a blank location
 vector<int> moveUp(vector<int> vec, int blank){
     vec[blank] = vec[blank - 4];
     vec[blank - 4] = 0;
@@ -51,9 +51,6 @@ int b5tob10(vector<int> vec){
 //Base 10 to base 5 conversion
 vector<int> b10tob5(int num){
     
-    cout << 'A' << endl << num << endl;
-    
-    cout << 'B' << endl;
     int i = 0;
     vector<int> result;
     double howdy;
@@ -62,7 +59,6 @@ vector<int> b10tob5(int num){
         cerr << "ERROR in base10 to base 5" << endl;
         return result;
     }
-    cout << 'B' << endl;
 
     while (num >= pow(5, i)){
         result.insert(result.begin(), int((num / (pow(5, i)))) % 5);
@@ -72,7 +68,7 @@ vector<int> b10tob5(int num){
     return result;
 }
 
-//Finding the iteration of the blank
+//Finding the iteration location of the blank
 int blankSpot(vector<int> state){
     
     int result = 0;
@@ -97,24 +93,28 @@ bool checkZeros(vector<int> vec){
     return false;
 }
 
-int BFS(vector<int> inState, vector<int> endState, int sol){
-    cout << "\n1 *";
+/*This algorithm takes in the input state, ending state, and an integer sol.
+     -the sol integer is a normal base 10 number that represents a sequence of moves
+     -This number is converted to base 5 and stored by digit in a vector
+     -each digit (1, 2, 3, 4) represents up, right, down, and left respectivly
+     -the sol integer is checked in the main function to make sure there are no 0s
+         in the base 5 version of the number since 0 does not represent any move
+ */
+vector<int> BFS(vector<int> inState, vector<int> endState, int sol){
+
     vector<int> tempState = inState;
-    cout << "\n2 * " << sol << endl;
     vector<int> check = b10tob5(sol);
-    cout << "\n3 *";
     int blank = blankSpot(tempState);
-    cout << "\n4 *";
+    vector<int> result;
     
-    cout << "\n\nsol: " << sol << endl;
-    for (int i = 0; i < check.size(); ++i){
-        cout << check[i] << " : ";
-    }
-    
-    //Get the current game state
+    //Get the game state according to the sequence of moves
+    // represented by integer sol
     for (int i = 0; i < check.size(); i++){
         
+        //Up
         if (check[i] == 1){
+            
+            //Make sure the blank is not at the top of the puzzle
             if (blank >= 4){
                 tempState = moveUp(tempState, blank);
                 blank = blankSpot(tempState);
@@ -124,7 +124,11 @@ int BFS(vector<int> inState, vector<int> endState, int sol){
                 break;
             }
         }
+        
+        //Right
         else if (check[i] == 2){
+            
+            //Make sure the blank is not at the right side of the puzzle
             if (blank % 4 != 3){
                 tempState = moveRight(tempState, blank);
                 blank = blankSpot(tempState);
@@ -134,7 +138,11 @@ int BFS(vector<int> inState, vector<int> endState, int sol){
                 break;
             }
         }
+        
+        //Down
         else if (check[i] == 3){
+            
+            //Make sure the blank is not at the bottom of the puzzle
             if (blank <= 11){
                 tempState = moveDown(tempState, blank);
                 blank = blankSpot(tempState);
@@ -144,7 +152,11 @@ int BFS(vector<int> inState, vector<int> endState, int sol){
                 break;
             }
         }
+        
+        //Left
         else if (check[i] == 4){
+            
+            //Make sure the blank is not at the left side of the puzzle
             if (blank % 4 != 0){
                 tempState = moveLeft(tempState, blank);
                 blank = blankSpot(tempState);
@@ -156,21 +168,26 @@ int BFS(vector<int> inState, vector<int> endState, int sol){
         }
     }
     
-    //Checking if input is the end state (base case)
-    if (tempState == endState && blank != -1)
-        return b5tob10(check);
-    
-    //if not, iterate to next valid solution sequence (a sequence with no 0s)
-    ++sol;
-    check = b10tob5(sol);
-    while(checkZeros(check)){
-            ++sol;
-            check = b10tob5(sol);
+    //Checking if the derived state is the end state, and if the move is valid
+    // valid: it does not move the blank off the puzzle
+    if (tempState == endState && blank != -1){
+        
+        //If we are at the end, return sol in location 0,
+        // and '1' in location 1 to represent that the
+        // number in location 0 represents the solution move sequence
+        result.push_back(sol);
+        result.push_back(1);
+        return result;
     }
-    
-    //Once we have the next sequence of moves in the BFS,
-    // return the next one checked
-    return BFS(inState, endState, sol);
+    else{
+        
+        //If we are NOT at the end, return sol in location 0,
+        // and 0 in location 1 to represent that the
+        // number in location 0 is not the solution to the puzzle
+        result.push_back(sol);
+        result.push_back(0);
+        return result;
+    }
 }
 
 int main(){
@@ -178,52 +195,65 @@ int main(){
     //Getting the input state
     string input;
     vector<int> inputState, endingState;
-//    cout << "Enter 15-puzzle starting state by rows, separated by commas (0 for blank):" << endl;
-//    cin >> input;
-//    stringstream in(input);
-//
-//    int i;
-//    while (in >> i){
-//        inputState.push_back(i);
-//        if (in.peek() == ',')
-//            in.ignore();
-//    }
-//    if(inputState.size() != 16){
-//        cout << "Invalid state entered.  Exiting" << endl;
-//        return 0;
-//    }
-//
-//    //Getting the ending state
-//    cout << "Enter ending state by rows, separated by commas (0 for blank):" << endl;
-//    cin >> input;
-//    stringstream end(input);
-//
-//    while (end >> i){
-//        endingState.push_back(i);
-//        if (end.peek() == ',')
-//            end.ignore();
-//    }
-//    if(endingState.size() != 16){
-//        cout << "Invalid state entered.  Exiting" << endl;
-//        return 0;
-//    }
-//
-//    if (inputState == endingState){
-//        cout << "Input and ending state are the same. exiting" << endl;
-//        return 1;
-//    }
-    inputState.push_back(1);inputState.push_back(2);inputState.push_back(3);inputState.push_back(4);
-    inputState.push_back(0);inputState.push_back(6);inputState.push_back(11);inputState.push_back(7);
-    inputState.push_back(5);inputState.push_back(9);inputState.push_back(10);inputState.push_back(8);
-    inputState.push_back(13);inputState.push_back(14);inputState.push_back(15);inputState.push_back(12);
-   
-    endingState.push_back(1);endingState.push_back(2);endingState.push_back(3);endingState.push_back(4);
-    endingState.push_back(5);endingState.push_back(6);endingState.push_back(7);endingState.push_back(8);
-    endingState.push_back(9);endingState.push_back(10);endingState.push_back(11);endingState.push_back(12);
-    endingState.push_back(13);endingState.push_back(14);endingState.push_back(15);endingState.push_back(0);
+    cout << "Enter 15-puzzle starting state by rows, separated by commas (0 for blank):" << endl;
+    cin >> input;
+    stringstream in(input);
+
+    int i;
+    while (in >> i){
+        inputState.push_back(i);
+        if (in.peek() == ',')
+            in.ignore();
+    }
+    if(inputState.size() != 16){
+        cout << "Invalid state entered.  Exiting" << endl;
+        return 0;
+    }
+
+    //Getting the ending state
+    cout << "Enter ending state by rows, separated by commas (0 for blank):" << endl;
+    cin >> input;
+    stringstream end(input);
+
+    while (end >> i){
+        endingState.push_back(i);
+        if (end.peek() == ',')
+            end.ignore();
+    }
+    if(endingState.size() != 16){
+        cout << "Invalid state entered.  Exiting" << endl;
+        return 0;
+    }
+
+    if (inputState == endingState){
+        cout << "Input and ending state are the same. exiting" << endl;
+        return 1;
+    }
     
-    int solution = 1;
-    solution = BFS(inputState, endingState, solution);
+    //Findin the solution
+    int sol = 1, numberOfStates = 1;
+    vector<int> solution = BFS(inputState, endingState, sol);
+    vector<int> check;
+
+    //BFS maps the given integer 'sol' to a sqeuence of moves
+    // by changing the number to base 5, and checking for 0s in
+    // the digits
+    while (solution[1] != 1){
+        
+        ++sol;
+        check = b10tob5(sol);
+        
+        //This checks the next sequence of moves for 0s,
+        // if there are 0s in the base 5 number, we have to try the next number
+        // since 0 doesn't represent any valid move for this implementation
+        while(checkZeros(check)){
+            ++sol;
+            check = b10tob5(sol);
+        }
+        
+        ++numberOfStates;
+        solution = BFS(inputState, endingState, sol);
+    }
     
     //Display the start state
     cout << "\nStart:\n";
@@ -236,7 +266,7 @@ int main(){
     
     //Print out the solution sequence of moves by applying the moves
     // to the input state
-    vector<int> solVec = b10tob5(solution);
+    vector<int> solVec = b10tob5(solution[0]);
     int blank = blankSpot(inputState);
     for (int i = 0; i < solVec.size(); i++){
         if (solVec[i] == 1){
@@ -273,7 +303,7 @@ int main(){
         }
         blank = blankSpot(inputState);
     }
-    cout << '\n';
+    cout << '\n' << "Generated " << numberOfStates << " states." << endl << endl;
     
     return 1;
 }
